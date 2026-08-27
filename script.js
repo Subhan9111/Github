8f9cc728456f730f5118777559e313f70940d8f4
Subhan9111 <monea007@osloskolen.no>
2026-08-27 14:31:58 +0200

    M,

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ let spiller2 = 0
 let mork = false
  
 modus.addEventListener("click", function(){
- 
     mork = !mork
- 
+
     if(mork == true){
         document.body.className = "mork"
         modus.textContent = "lys modus"
@@ -18,30 +17,23 @@ modus.addEventListener("click", function(){
         document.body.className = ""
         modus.textContent = "mørk modus"
     }
- 
 })
  
 document.getElementById("pluss1").addEventListener("click", function(){
- 
     spiller1 = spiller1 + 1
     poeng.textContent = spiller1
- 
 })
  
 document.getElementById("minus1").addEventListener("click", function(){
- 
     if(spiller1 > 0){
         spiller1 = spiller1 - 1
         poeng.textContent = spiller1
     }
- 
 })
  
 document.getElementById("pluss2").addEventListener("click", function(){
- 
     spiller2 = spiller2 + 1
     poeng2.textContent = spiller2
- 
 })
  
 document.getElementById("minus2").addEventListener("click", function(){
@@ -62,7 +54,6 @@ document.getElementById("nullstill").addEventListener("click", function(){
     poeng2.textContent = spiller2
  
 })
- 
 fargebytter.addEventListener("click", function(){
  
     if(document.body.className == "glad"){
